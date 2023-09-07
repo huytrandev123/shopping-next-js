0b6676e6d3bc8a0157b3114f49adb77e28cccb78
huytrandev123 <trandoquanghuy123@gmail.com>
2023-09-07 22:48:40 +0700

    4. Home UI

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import styles from './page.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 function About() {
   return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './page.module.scss';
+import Hero from 'public/hero.png'
+import Image from 'next/image';
+
+const cx = classNames.bind(styles);
 
 function Home() {
-    return <div>Home</div>;
+    return (
+        <div className={cx('container')}>
+            <div className={cx('item')}>
+                <h1 className={cx('title')}>Better design for you digital products</h1>
+                <p className={cx('description')}>Turning your idea intro Reality. We bring together the teams from the global tech industry</p>
+                <button className={cx('button')}>See our works</button>
+            </div>
+            <div className={cx('item')}>
+                <Image src={Hero} className={cx('img')} /> 
+            </div>
+        </div>
+    );
 }
 
 export default Home;
