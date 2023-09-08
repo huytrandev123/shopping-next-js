8ab5e53d4830e465fd7a968c109db8aca60aa8ab
huytrandev123 <trandoquanghuy123@gmail.com>
2023-09-08 15:23:59 +0700

    6. Contact UI

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,53 @@
-'use client'
+'use client';
 import React from 'react';
-import styles from './Contact.module.scss'
+import styles from './Contact.module.scss';
 import classNames from 'classnames/bind';
+import Image from 'next/image';
+import Button from '@/components/Button';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function Contact() {
-    return <div className={cx('contact')}>Contact</div>;
+    return (
+        <div className={cx('container')}>
+            <h1 className={cx('title')}>Let's keep in touch</h1>
+            <div className={cx('content')}>
+                {/* Img */}
+                <div className={cx('imgContainer')}>
+                    <Image 
+                        src="/contact.png"
+                        alt="contact img"
+                        fill={true}
+                        sizes={15}
+                        className={cx('image')}
+                    /> 
+                </div>  
+
+                {/* Form */}
+                <form className={cx('form')}>
+                    <input  
+                        className={cx('input')}
+                        type='text'
+                        placeholder='name'
+                    /> 
+
+                    <input  
+                        className={cx('input')}
+                        type='email'
+                        placeholder='email'
+                    /> 
+
+                    <textarea 
+                        className={cx('textArea')} 
+                        placeholder='message'
+                        cols="30" 
+                        rows="10">    
+                    </textarea>
+                    <Button url="#" text="Send" />
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
